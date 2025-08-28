8c8bc5ab3a8683300a9d7884d47c24b5e54f886e
CENTIC <CENTIC@10.6.66.139>
2025-08-28 11:51:50 -0500

    fgd

--- src/estruturas/Lista.java
+++ src/estruturas/Lista.java
@@ -15,4 +15,15 @@ public class Lista {
         nodoIni.siguiente = cabeza; 
         cabeza = nodoIni; 
     }
+     
+     public void insertarNodoFinal(int dato){
+         Nodo nodoFin = new Nodo(dato);
+         Nodo nodoRecorre = cabeza; // hise una copia 
+         
+         while (nodoRecorre.siguiente != null){
+             nodoRecorre = nodoRecorre.siguiente;
+         }
+         
+         nodoRecorre.siguiente = nodoFin;
+     }
 }
