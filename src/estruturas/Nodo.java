2fc215e601cc59b5285edc1a9542d9cd1e06aa07
CENTIC <CENTIC@10.6.66.139>
2025-08-28 11:28:13 -0500

    fgd

---
+++ src/estruturas/Nodo.java
@@ -0,0 +1,20 @@
+
+package estruturas;
+
+public class Nodo {
+    
+     public int dato;
+    public Nodo siguiente = null;
+    
+    public Nodo(int dato){
+        this.dato = dato; 
+    }
+  
+    
+    public static void main(String[] args) {
+        
+        
+       
+    }
+    
+}
